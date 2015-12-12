a0950ec9b613ee44eb71a9e73d5036de67efe498
ryym <ryym.64@gmail.com>
2015-12-12 10:36:07 +0900

    Implement most basic functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
-// TODO
+const marked = require('marked');
+
+const EL = {
+  display: document.getElementById('display')
+};
+
+function handleDragOver(event) {
+  event.preventDefault();
+  event.dataTransfer.dropEffect = 'copy';
+}
+
+/**
+ * Convert the dropped file
+ * and display it as HTML.
+ */
+function convertMarkdown(event) {
+  event.stopPropagation();
+  event.preventDefault();
+  const file   = event.dataTransfer.files[0];
+  const reader = new FileReader();
+
+  reader.addEventListener('load', (e) => {
+    EL.display.innerHTML = marked(e.target.result);
+  });
+  reader.readAsText(file, 'utf-8');
+}
+
+EL.display.addEventListener('dragover', handleDragOver);
+EL.display.addEventListener('drop', convertMarkdown);
